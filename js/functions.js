d46a2c3597b655077a0b9e5c2915a5a863a35394
Emilio Gallo <emilio.gallo17@gmail.com>
2024-12-18 11:54:35 +0100

    feat: created and implemented printCalendarDays( ) function - js/functions.js

--- js/functions.js
+++ js/functions.js
@@ -1,1 +1,54 @@
 console.log('///// Functions /////');
+
+/**
+ * Description: function calls the given function [functionToCall] only after time param [delay].
+ * @param {function} functionToCall
+ * @param {number} delay
+ * @returns {function} 
+ */
+function debounce(functionToCall, delay) {
+
+   // Define variable for timeout
+   let timeout;
+
+   return function (...args) { // capture arguments passed to the debounced function
+
+      // Clear the previous timer
+      clearTimeout(timeout);
+
+      // Capture context of function
+      const context = this;
+
+      // Set a new timeout to call the function
+      timeout = setTimeout(() => {
+
+         // Execute the function with the correct context and arguments
+         functionToCall.apply(context, args);
+
+      }, delay);
+
+   };
+
+};
+
+/**
+ * Description: function prints on DOM element [cardsContainer] days from a given array.
+ * @param {array} days
+ * @param {obj} cardsContainer
+ */
+function printCalendarDays(days, cardsContainer) {
+
+   // Clear previous list elements
+   cardsContainer.innerHTML = '';
+
+   days.forEach((day) => {
+
+      cardsContainer.innerHTML += `
+         <div class="card ${day.status} day-${day.dayValue}">
+            ${day.dayValue}
+         </div>
+      `
+
+   });
+
+};
